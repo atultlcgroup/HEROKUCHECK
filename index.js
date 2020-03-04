75e270e1b143d0d015e2151cff874e6d055bdf3a
atultlcgroup <atul.srivastava@tlcgroup.com>
2020-03-04 11:27:12 +0530

    done changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let body_praser = require("body-parser")
 let app = express();
 app.use(cors())
 app.use(body_praser())
-let port = config.PORT;
+let port = process.env.PORT || config.PORT;
 app.use("/user" , userRoute)
 app.use("/", (req , res)=>{
   res.status(200).send(`SERVER started AT ${port}`)
