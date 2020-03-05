48272b689ee9a2d854a6b9eecb58773c654f0038
atultlcgroup <atul.srivastava@tlcgroup.com>
2020-03-05 10:02:14 +0530

    environment setup done for this project

--- config.js
+++ config.js
@@ -1,3 +1,23 @@
+
+let NODE_ENV = process.env.NODE_ENV || 'development';
+let ENV_OBJ ;
+switch (NODE_ENV) {
+    case 'development':
+        ENV_OBJ = {PORT: 4000 }
+        break;
+    case 'microtest':
+        ENV_OBJ = {PORT: 5000 }
+        break;
+    case 'production':
+        console.log(`ggggg`)
+        ENV_OBJ = {PORT: 6000 }
+            break;
+    default:
+        console.log(`ggggg1`)
+        ENV_OBJ = {PORT: 7000 }
+        break;
+}
+console.log(ENV_OBJ)
 module.exports={
-    PORT : 5000
+    ENV_OBJ
 }
