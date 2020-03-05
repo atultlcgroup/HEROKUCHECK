1ce8e3cd01bcd5810e2d11c9d240d53d8f425538
atultlcgroup <atul.srivastava@tlcgroup.com>
2020-03-05 10:05:10 +0530

    comment removed

--- config.js
+++ config.js
@@ -9,15 +9,12 @@ switch (NODE_ENV) {
         ENV_OBJ = {PORT: 5000 }
         break;
     case 'production':
-        console.log(`ggggg`)
         ENV_OBJ = {PORT: 6000 }
             break;
     default:
-        console.log(`ggggg1`)
         ENV_OBJ = {PORT: 7000 }
         break;
 }
-console.log(ENV_OBJ)
 module.exports={
     ENV_OBJ
 }
