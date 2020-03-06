c6fea26845428a550c6b608dafc33031c8fba1e5
atultlcgroup <atul.srivastava@tlcgroup.com>
2020-03-06 17:35:58 +0530

    pg added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let helmet = require('helmet')
 let db = require("./databases/db")
 let auth = require("./auth/authenticate")
 let chatRoute = require('./routers/chat')
+let pool = require('./databases/pg')
 let app = express();
 app.use(cors())
 app.use(helmet())
@@ -16,6 +17,21 @@ let port = process.env.PORT ||  config.PORT;
 app.use("/user" ,userRoute)
 app.use("/api/user" ,userRoute)
 app.use("/api/chat" ,chatRoute)
+
+
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 app.use("/", (req , res)=>{
   res.status(200).send(`SERVER started AT ${port}`)
